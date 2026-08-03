7b7230dfd6a8ec5fbfbcadb8d4af99494c8922cb
ClarkGodwin <clarkniyonzima@gmail.com>
2026-08-03 21:40:31 +0200

    router configured and ready to use

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,21 @@ import { Settings, CirclePlay } from '@lucide/vue';
 </script>
 
 <template>
-  <section id="header" class="text-frosted text-center text-header flex gap-home justify-center items-center *:cursor-pointer">
-    <h1>Fluid Clamp</h1>
-    <CirclePlay class="size-(--text-header)"/>
-    <Settings class="size-(--text-header)"/>
-  </section>
+  <header id="header" class="text-frosted text-center text-header flex gap-home justify-center items-center">
+    <RouterLink :to="{name: 'home'}">Fluid Clamp</RouterLink>
+    
+    <RouterLink :to="{ name: 'clamp'}">
+      <CirclePlay class="size-(--text-header)"/>
+    </RouterLink>
 
-  <section id="home" class="my-5 bg-surface-300 w-full rounded-2xl py-py-home px-px-home text-home *:text-frosted">
-    Welcome in our <span>Fluid Clamp</span> website. <br>
-    Its goal is to make the design responsivness a bit easier for developpers.
-    It's always annoying how we have to change the value of an element (either its width, height, gap, text's size) so that it looks clean on all screen sizes <br> <br> <br>
+    <RouterLink :to="{ name: 'settings'}">
+      <Settings class="size-(--text-header)"/>
+    </RouterLink>
 
-    <span>How it works : </span> <br> <br>
+  </header>
 
-    <span>* </span> The values on which the calculation is based is expressed only in pixels. <br> <br>
+  <RouterView/>
 
-    <span>* </span> You first pick the maximum and the minimum screen size for your web app. <br> To be more specific, above the maximum (you chose) on which the look of the website won't change and same for under the minimum <br> <br>  
-
-    <span>* </span> You then pick the element'size corresponding to the maximum and then the minimum screen size <br> <br>
-
-    <span>* </span> Click on the clamp button, the clamp value will be given to you, copy it, give it to the element and its size will adjust across the screen sizes you chose
-
-  </section>
 </template>
 
 <style scoped></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,20 @@
-<script setup></script>
+<script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <section class="my-5 bg-surface-300 w-full rounded-2xl py-py-home px-px-home text-home *:text-frosted">
+    Welcome in our <span>Fluid Clamp</span> website. <br>
+    Its goal is to make the design responsivness a bit easier for developpers.
+    It's always annoying how we have to change the value of an element (either its width, height, gap, text's size) so that it looks clean on all screen sizes <br> <br> <br>
+
+    <span>How it works : </span> <br> <br>
+
+    <span>* </span> The values on which the calculation is based is expressed only in pixels. <br> <br>
+
+    <span>* </span> You first pick the maximum and the minimum screen size for your web app. <br> To be more specific, above the maximum (you chose) on which the look of the website won't change and same for under the minimum <br> <br>  
+
+    <span>* </span> You then pick the element'size corresponding to the maximum and then the minimum screen size <br> <br>
+
+    <span>* </span> Click on the clamp button, the clamp value will be given to you, copy it, give it to the element and its size will adjust across the screen sizes you chose
+
+  </section>
+</template>
