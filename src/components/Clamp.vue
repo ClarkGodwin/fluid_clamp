f5af6cf937292135dbb3f95731b225afcbd1f376
ClarkGodwin <clarkniyonzima@gmail.com>
2026-08-03 22:16:04 +0200

    elements sizes in the clamp added

--- src/components/Clamp.vue
+++ src/components/Clamp.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <section class="bg-surface-300 text-[14px] sm:text-clamp rounded-2xl flex flex-col gap-5 sm:gap-0 sm:flex-row justify-between items-center py-py-section px-px-section">
+    <section class=" mb-7 bg-surface-300 text-[14px] sm:text-clamp rounded-2xl flex flex-col gap-5 sm:gap-0 sm:flex-row justify-between items-center py-py-section px-px-section">
         <h2 class="text-frosted">Screen Sizes : </h2>
         <span>
             <label for="">Max </label>
@@ -10,6 +10,18 @@
         <span>
             <label for="">Min </label>
             <input type="number" name="" id="" min="0" value="320">
+        </span>
+    </section>
+
+    <section class="bg-surface-300 text-[14px] sm:text-clamp rounded-2xl flex flex-col gap-5 sm:gap-0 sm:flex-row justify-between items-center py-py-section px-px-section">
+        <h2 class="text-frosted">Element Sizes : </h2>
+        <span>
+            <label for="">Max </label>
+            <input type="number" name="" id="" value="0">
+        </span>
+        <span>
+            <label for="">Min </label>
+            <input type="number" name="" id="" min="0" value="0">
         </span>
     </section>
 </template>
